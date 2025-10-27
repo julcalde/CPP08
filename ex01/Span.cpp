f5f0674de8dbd965ad8bed339c81dfaaddb9bf96
julcalde <julcalde@student.42heilbronn.de>
2025-10-27 21:08:42 +0100

    ex01: [Span.cpp] methods should work this way.

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 15:08:08 by julcalde          #+#    #+#             */
-/*   Updated: 2025/10/27 15:19:23 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/10/27 21:07:56 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,57 @@ Span& Span::operator=(const Span& other)
 Span::~Span()
 {
 	std::cout << "Span destructor called." << std::endl;
+}
+
+/*
+	addNumber: Adds a single number to the Span.
+*/
+void Span::addNumber(int num)
+{
+	if (_numbers.size() >= _maxSize)
+		throw std::runtime_error("Span is full");
+	_numbers.push_back(num);
+}
+
+/*
+	addNumber (template): Adds a range of numbers to the Span.
+*/
+template <typename Iter>
+void Span::addNumber(Iter begin, Iter end)
+{
+	size_t count = std::distance(begin, end);
+	if (_numbers.size() + count > _maxSiza)
+		throw std::runtime_error("Max capacity exceeded");
+	_numbers.insert(_numbers.end(), begin, end);
+}
+
+/*
+	shortestSpan: Finds the shortest span (or distance) between any two numbers in the Span.
+*/
+int Span::shortestSpan() const
+{
+	if (_numbers.size() <= 1)
+		throw std::runtime_error("Not enough numbers to find a span");
+	std::vector<int> sorted = _numbers;
+	std::sort(sorted.begin(), sorted.end());
+	int minSpan = std::abs(sorted[1] - sorted[0]);
+	for (size_t i = 2; i < sorted.size(); ++i)
+	{
+		int span = std::abs(sorted[i] - sorted[i - 1]);
+		if (span < minSpan)
+			minSpan = span;
+	}
+	return (minSpan);
+}
+
+/*
+	longestSpan: Finds the longest span (or distance) between any two numbers in the Span.
+*/
+int Span::longestSpan() const
+{
+	if (_numbers.size() <= 1)
+		throw std::runtime_error("Not enough numbers to find a span");
+	std::vector<int> sorted = _numbers;
+	std::sort(sorted.begin(), sorted.end());
+	return (std::abs(sorted.back() - sorted.front()));
 }
