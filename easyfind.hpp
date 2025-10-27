92774fa71748dcb153641a665d7b098aa7708a47
julcalde <julcalde@student.42heilbronn.de>
2025-10-27 14:43:45 +0100

    ex00: [easyfind.hpp] added function to look for instance of an integer inside of a container and return either the iterator of the first occurrence or a runtime error (if not value not found)

---
+++ easyfind.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 14:34:45 by julcalde          #+#    #+#             */
+/*   Updated: 2025/10/27 14:40:48 by julcalde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <algorithm>
+#include <stdexcept>
+
+/*
+	easyfind: A template function that searches for a given integer value
+	in a container (like std::vector, std::list, etc.) and returns an iterator
+	to the first occurrence of that value. If the value is not found, it throws a
+	std::runtime_error exception.
+*/
+template <typename T>
+typename T::iterator easyfind(T& container, int value)
+{
+	typename T::iterator iter = std::find(container.begin(), container/end(), value);
+	if (iter == container.end())
+		throw std::runtime_error("Value not present in container");
+	return (iter);
+}
+
+#endif
