5a8df46f759bb1e746b9b5c7554e5502f3a016a7
julcalde <julcalde@student.42heilbronn.de>
2025-10-27 15:04:28 +0100

    ex00: [main.cpp] added test with std::list.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 14:44:03 by julcalde          #+#    #+#             */
-/*   Updated: 2025/10/27 14:53:40 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/10/27 15:00:58 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,26 @@ int main(void)
 		std::cout << e.what() << std::endl;
 	}
 
+	// Tests with std::list
+	std::list<int> lst;
+	for (int i = 10; i <= 40; i += 10)
+		lst.push_back(i);
+	
+	try
+	{
+		std::list<int>::iterator iter = easyfind(lst, 20);
+		std::cout << "AAAFound value: " << *iter << " in list." << std::endl;
+		iter = easyfind(lst, 40);
+		std::cout << "BBBFound value: " << *iter << " in list." << std::endl;
+		iter = easyfind(lst, 60);
+		std::cout << "CCCFound value: " << *iter << " in list." << std::endl;
+		iter = easyfind(lst, 40);
+		std::cout << "DDDFound value: " << *iter << " in list." << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 	
 	return (0);
 }
