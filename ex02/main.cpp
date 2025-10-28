eb1bb3bc589a9b8027a1bb6e66bff3356dd6b8f0
julcalde <julcalde@student.42heilbronn.de>
2025-10-28 15:54:00 +0100

    ex02: [main.cpp] Added test to iterate through a std::stack that copied the mutantstack. it shows the top element and pops it until the stack is emptied. Added comments to the tests.

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 21:37:16 by julcalde          #+#    #+#             */
-/*   Updated: 2025/10/28 14:23:56 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/10/28 15:49:00 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,46 +18,55 @@ int main(void)
 {
 	// Guidelines example test
 	std::cout << "--- Guidelines example test ---" << std::endl;
-	MutantStack<int> mustache;
-	mustache.push(5);
+	MutantStack<int> mustache; // create a MutantStack of integers
+	mustache.push(5); // bottom of the stack and first element
 	mustache.push(17);
-	std::cout << mustache.top() << std::endl;
-	mustache.pop();
-	std::cout << mustache.size() << std::endl;
+	std::cout << mustache.top() << std::endl; // 17
+	mustache.pop(); // removes 17 from the top of the stack
+	std::cout << mustache.size() << std::endl; // 1
 	mustache.push(3);
 	mustache.push(5);
 	mustache.push(737);
 	mustache.push(0);
-	MutantStack<int>::iterator it = mustache.begin();
-	MutantStack<int>::iterator ite = mustache.end();
-	++it;
-	--it;
-	while (it != ite)
+	MutantStack<int>::iterator it = mustache.begin(); // points to 5
+	MutantStack<int>::iterator ite = mustache.end(); // points to one past 0
+	++it; // points to 3
+	--it; // points back to 5
+	while (it != ite) // iterate through the stack
 	{
 		std::cout << *it << std::endl;
 		++it;
 	}
-
+	
 	// Comparison with std::list
 	std::cout << "--- Comparison with std::list ---" << std::endl;
-	std::list<int> lst;
-	lst.push_back(5);
+	std::list<int> lst; // create a list of integers
+	lst.push_back(5); // bottom of the list and first element
 	lst.push_back(17);
-	std::cout << lst.back() << std::endl;
-	lst.pop_back();
-	std::cout << lst.size() << std::endl;
+	std::cout << lst.back() << std::endl; // 17
+	lst.pop_back(); // removes 17 from the top of the list
+	std::cout << lst.size() << std::endl; // 1
 	lst.push_back(3);
 	lst.push_back(5);
 	lst.push_back(737);
 	lst.push_back(0);
-	std::list<int>::iterator lst_it = lst.begin();
-	std::list<int>::iterator lst_ite = lst.end();
-	++lst_it;
-	--lst_it;
-	while (lst_it != lst_ite)
+	std::list<int>::iterator lst_it = lst.begin(); // points to 5
+	std::list<int>::iterator lst_ite = lst.end(); // points to one past 0
+	++lst_it; // points to 3
+	--lst_it; // points back to 5
+	while (lst_it != lst_ite) // iterate through the list
 	{
 		std::cout << *lst_it << std::endl;
 		++lst_it;
+	}
+	
+	// this part is to test the copy constructor
+	std::cout << "--- Copy constructor test ---" << std::endl;
+	std::stack<int> s(mustache); // create a standard stack by copying mustache
+	while (!s.empty()) // iterate through the stack
+	{
+		std::cout << s.top() << std::endl; // print the top element
+		s.pop(); // remove the top element
 	}
 	
 	return (0);
